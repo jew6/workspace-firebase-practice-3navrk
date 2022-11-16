d9b2f106470927bf9d17949cf7bd66cd3da7434d
jew6 <111775028+jew6@users.noreply.github.com>
2022-11-16 12:23:57 -0500

    Jonathan Walker 11/16 update

--- js/SaveDisplay.js
+++ js/SaveDisplay.js
@@ -1,6 +1,15 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {};
+var firebaseConfig = {
+  apiKey: 'AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM',
+  authDomain: 'grocerylist-91956.firebaseapp.com',
+  databaseURL: 'https://grocerylist-91956.firebaseio.com',
+  projectId: 'grocerylist-91956',
+  storageBucket: 'grocerylist-91956.appspot.com',
+  messagingSenderId: '813812426276',
+  appId: '1:813812426276:web:93e5897af12892ff78dab1',
+  measurementId: 'G-VZ83BTR72T',
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -9,7 +18,17 @@ $(".sampleSurvey input[type='submit']").click(function(e) {
   e.preventDefault();
 
   // get the value of the form using serializeArray method
-  
+  //get the value of form
+  var inputdata = $('form').serializeArray();
+  var data={}; // data sent to the database
+  console.log(inputdata[2]);
+  console.log(inputdata[2].name);
+  console.log(inputdata[2].value);
+  /* save the data to database */
+  inputdata.forEach((entry)=>{
+    console.log(entry);
+    data[entry.name]=entry.value;
+  });
 
 
 
