0c09b341331907210193a13a22b8b0b3d148cb35
jew6 <111775028+jew6@users.noreply.github.com>
2022-11-21 13:00:52 -0500

    2 files modified

--- js/Signin.js
+++ js/Signin.js
@@ -45,3 +45,34 @@ $('#Login').submit(function (e) {
     });
 });
 
+$('#google').click(function(){
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then((result)=>{
+    const user = result.user;
+    console.log("Google User "+user.email+" Log in");
+  }).catch((error)=>{
+    console.log("Error Message "+error.message);
+  });
+});
+
+/*import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });*/
